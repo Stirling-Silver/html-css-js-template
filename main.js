c8c58183279e84eca33aad46d5523dabf91c2939
Stirling Luke <s.luke@EDU.epsb.ca>
2022-09-29 10:27:53 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-document.getElementById("buttonfunc").addEventListener ("click", myFunction);
+document.getElementById("button").addEventListener("click", myFunction);
 
 function myFunction() {
 
+    console.log
 let CSS1G = +document.getElementById("CSS1").value
 
 let SP1G = +document.getElementById("SP1").value
@@ -14,7 +15,6 @@ let PAG = +document.getElementById("PA").value
 
 
 document.getElementById("Addition").innerHTML = (CSS1G + SP1G + SP2G + CSS2G + PAG);
-
 
 let FinalGrade= (CSS1G + SP1G + SP2G + CSS2G + PAG) / 5;
 
