2f343f7fcf2189065e2c01203e1a2300a2a4cf7f
Stirling Luke <s.luke@EDU.epsb.ca>
2022-10-03 10:31:36 -0600

    Relearning

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
-document.getElementById("button").addEventListener("click", myFunction);
+//document.getElementById("button").addEventListener("click", myFunction);
 
-function myFunction() {
+//function myFunction() {
 
-    console.log
-let CSS1G = +document.getElementById("CSS1").value
+  //  console.log
+//let CSS1G = +document.getElementById("CSS1").value
 
-let SP1G = +document.getElementById("SP1").value
+//let SP1G = +document.getElementById("SP1").value
 
-let SP2G = +document.getElementById("SP2").value
+//let SP2G = +document.getElementById("SP2").value
 
-let CSS2G = +document.getElementById("CSS2G").value
+//let CSS2G = +document.getElementById("CSS2G").value
 
-let PAG = +document.getElementById("PA").value
-
-
-document.getElementById("Addition").innerHTML = (CSS1G + SP1G + SP2G + CSS2G + PAG);
-
-let FinalGrade= (CSS1G + SP1G + SP2G + CSS2G + PAG) / 5;
+//let PAG = +document.getElementById("PA").value
 
 
-document.getElementById("output").innerHTML = FinalGrade;
+//document.getElementById("Addition").innerHTML = (CSS1G + SP1G + SP2G + CSS2G + PAG);
+
+//let FinalGrade= (CSS1G + SP1G + SP2G + CSS2G + PAG) / 5;
+document.getElementById("btn").addEventListener("click", helloButton);
+
+function helloButton() {
+    alert("Hello World");
+    alert("Hello World Again");
 }
+
+
+
+//document.getElementById("output").innerHTML = FinalGrade;
+//}
